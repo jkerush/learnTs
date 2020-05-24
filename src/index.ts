6248891b89c0221850b74089545a7faa7ec35dff
josh <kerush@163.com>
2020-05-24 22:26:59 +0800

    add interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import axios from 'axios'
+
+
 let sty: string = "Studing the TypeScript now...";
 let app = document.querySelector("#app")
-app.innerHTML= sty;
+app.innerHTML = sty;
 // 原始类型
 
 let bool: boolean = true;
@@ -11,6 +14,11 @@ let str: string = "this is String";
 
 let arr: number[] = [12, 23, 354, 256];
 let arr2: Array<number | string> = [23, "sdk", 25, "slid", "8iesk"];
+
+arr2.map(item => {
+    console.log(item)
+})
+
 
 // 元祖
 
@@ -23,12 +31,12 @@ console.log(tuple);
 
 let func = (a: number, b: number) => a + b;
 let sfunc: (a: string, b: number) => string = (x: string, y: number) =>
-  `we have ${2 * y} ${x}`;
+    `we have ${2 * y} ${x}`;
 console.log(sfunc("joker", 2));
 
 // 对象
 
-let obj: { a: number; b: number } = { a: 3, b: 6 };
+let obj: { a: number; b: number } = {a: 3, b: 6};
 obj.b = 9;
 console.log(obj.a, obj.b);
 
@@ -46,7 +54,7 @@ console.log(undif === nuls);
 //void
 
 let nore = (a: number, b: number): void => {
-  console.log(a, b);
+    console.log(a, b);
 };
 
 // any
@@ -58,22 +66,46 @@ x = "kjik";
 // never
 
 let errors = (): never => {
-  throw new Error();
+    throw new Error();
 };
 
 // enum
 
 enum surType {
-  kokia = 1,
-  kokis,
-  kokim,
-  kokil,
+    kokia = 1,
+    kokis,
+    kokim,
+    kokil,
 }
+
 console.log(surType.kokia);
 console.log(surType[1]);
 
 enum strs {
-  succ = "you did it",
-  fail = "you miss it",
+    succ = "you did it",
+    fail = "you miss it",
 }
+
 console.log(strs.succ);
+
+
+// 接口和函数
+interface imgObj {
+    id: string,
+    author: string,
+    width: number,
+    height: number,
+    url: string
+}
+
+axios.get("https://picsum.photos/v2/list?limit=10").then((r: any) => {
+    console.log('result:', r)
+    if (r.status === 200) {
+        let imag_arcs: imgObj[] = r.data
+        console.log('the images array is ', imag_arcs)
+    }
+
+})
+
+
+
